da1b56a227e7be162502764cd872aa104bbed1f0
danielsnb-cryto <danielsebastiannieves09@gmail.com>
2026-03-13 06:50:10 -0500

    2

---
+++ Ejercicio/EjercicioBasicos/ejercicio_3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repaso_1.repaso
+{
+    public class ejercicio_3
+    {
+        public static void ejercutar()
+        {
+            const int AFORO_MAXIMO = 50;
+            int personasActuales = 0;
+
+            Console.WriteLine("--- Control de Aforo: Discoteca ---");
+            Console.WriteLine($"Capacidad máxima: {AFORO_MAXIMO} personas");
+            Console.WriteLine("-----------------------------------");
+
+            Console.Write("¿Cuántas personas desean ingresar?: ");
+            int personasNuevas = Convert.ToInt32(Console.ReadLine());
+
+            int totalProyectado = personasActuales + personasNuevas;
+
+            if (totalProyectado <= AFORO_MAXIMO)
+            {
+
+                Console.WriteLine("\n¡Bienvenidos! Pueden ingresar. Disfruten la noche. ");
+            }
+            else
+            {
+                int personasQueDebenSalir = totalProyectado - AFORO_MAXIMO;
+
+                Console.WriteLine("\nLo sentimos, el aforo está completo.");
+                Console.WriteLine($"Para que este grupo pueda entrar, deben salir primero {personasQueDebenSalir} personas.");
+                Console.WriteLine("Por favor, esperen en la fila.")
+                   }
+        }
+    }
+}
+
