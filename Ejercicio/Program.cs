272edf0d63eb0636a9f92fe03355eac346ffec61
danielsnb-cryto <danielsebastiannieves09@gmail.com>
2026-03-13 06:46:29 -0500

    upd

---
+++ Ejercicio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //pidele a chatgpt que te llame las clases y las ponga aqui en el main
+        }
+    }
+}
